715499e51ee308c1742600cab821bce7aa05a74d
Michael Friis <friism@gmail.com>
2015-03-05 14:46:51 -0800

    Add console app

---
+++ src/TestConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
